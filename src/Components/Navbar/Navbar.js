e2b0d1ddebd1f23dd031bf1b7b845a43b5d1d893
thummasreddy <63746972+thummasreddy@users.noreply.github.com>
2024-05-08 10:39:11 -0400

    Intial commit

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
-import logo from '../Assets/r_g_logo_01.png'
-import basket from '../Assets/Icons/basket.png'
+import logo from '../Assets/r_g_logo_01.png';
+import basket from '../Assets/Icons/basket.png';
 
 const Navbar = () => {
+
+  const [menu, setMenu] = useState ("shop")
   return (
     <div className='navbar'>
       <div className='nav-logo'>
         <img src={logo} alt=""/>
       </div>
-      <div className="nav-menu">
-        <div className="search">
+        <ul className="nav-menu">
+          <li onClick={()=>{setMenu("home")}}>Home{menu==="home"?<hr/>:<></>}</li>
+          <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
+          <li onClick={()=>{setMenu("about us")}}>About Us{menu==="about us"?<hr/>:<></>}</li>
+          <li onClick={()=>{setMenu("contact us")}}>Contact Us{menu==="contact us"?<hr/>:<></>}</li>
+      </ul>
+
+      <div className="search">
           <input type="text" placeholder="Search for products"/>
           <button className="btn btn-primary" type="button">Search</button>
         </div>
-      </div>
-      <div className="nav-account">
-        <button>Login</button>
+
+      <div className="nav-signup-basket">
         <button>SignUp</button>
-        <div className="basket-box">
-          <img src={basket} alt="Basket"/>
-        </div>
+        <img src= {basket} alt=""/>
+        <div className="nav-basket-count">0</div>
+      
+
       </div>
     </div>
   )
