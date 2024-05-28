397c7a02a2fa9bb37da342d33d0ae58affad797f
thummasreddy <63746972+thummasreddy@users.noreply.github.com>
2024-05-27 21:01:41 -0400

    NavBAr changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import ShopCategory from './Pages/ShopCategory';
+import LoginSignup from './Pages/LoginSignup';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
 
 function App() {
   return (
     <div>
+
+      <BrowserRouter>
       <Navbar/>
+      <Routes>
+
+        <Route path='/' element={<Home/>}/>
+        <Route path='/shop' element={<ShopCategory category="shopcategory"/>}/>
+        <Route path='/vegetables' element={<ShopCategory category="vegetables"/>}/>
+        <Route path='/fruits' element={<ShopCategory category="fruits"/>}/>
+        <Route path='/product' element={<Product/>}>
+
+          <Route path=':productId' element={<Product/>}/>
+        </Route>
+      
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
+
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,36 +2,39 @@ import React, { useState } from 'react';
 import './Navbar.css';
 import logo from '../Assets/r_g_logo_01.png';
 import basket from '../Assets/Icons/basket.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState("shop"); // Using more descriptive state name
 
-  const [menu, setMenu] = useState ("shop")
+  const handleMenuClick = (menu) => {
+    setActiveMenu(menu);
+  };
+
   return (
     <div className='navbar'>
       <div className='nav-logo'>
-        <img src={logo} alt=""/>
+        <img src={logo} alt="Logo"/>
       </div>
-        <ul className="nav-menu">
-          <li onClick={()=>{setMenu("home")}}>Home{menu==="home"?<hr/>:<></>}</li>
-          <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
-          <li onClick={()=>{setMenu("about us")}}>About Us{menu==="about us"?<hr/>:<></>}</li>
-          <li onClick={()=>{setMenu("contact us")}}>Contact Us{menu==="contact us"?<hr/>:<></>}</li>
+      <ul className="nav-menu">
+        <li onClick={() => handleMenuClick("home")}><Link to='/'> Home</Link>{activeMenu === "home" && <hr/>}</li>
+        <li onClick={() => handleMenuClick("shop")}><Link to='/shop'> Shop</Link>{activeMenu === "shop" && <hr/>}</li>
+        <li onClick={() => handleMenuClick("vegetables")}><Link to='/vegetables'> Vegetables</Link>{activeMenu === "vegetables" && <hr/>}</li>
+        <li onClick={() => handleMenuClick("fruits")}><Link to='/fruits'> Fruits</Link>{activeMenu === "fruits" && <hr/>}</li>
       </ul>
 
       <div className="search">
-          <input type="text" placeholder="Search for products"/>
-          <button className="btn btn-primary" type="button">Search</button>
-        </div>
+        <input type="text" placeholder="Search for products"/>
+        <button className="btn btn-primary" type="button">Search</button>
+      </div>
 
       <div className="nav-signup-basket">
-        <button>SignUp</button>
-        <img src= {basket} alt=""/>
+        <Link to='/signup'><button>SignUp</button></Link> 
+        <Link to='/cart'><img src={basket} alt="Basket"/></Link>
         <div className="nav-basket-count">0</div>
-      
-
       </div>
     </div>
-  )
+  );
 }
 
 export default Navbar;
