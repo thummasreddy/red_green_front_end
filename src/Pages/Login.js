32d72ec564ae2fdf1459bb5ba873e445b420e5f1
thummasreddy <63746972+thummasreddy@users.noreply.github.com>
2024-05-30 15:04:04 -0400

    Login_Page CSS

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -4,18 +4,33 @@ import '../Components/Styles/Login.css';
 const Login = () => {
   return (
     <div className="login-container">
-      <h1>Login Page</h1>
-      <form>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input type="text" id="username" name="username" />
+      <div className="login-box">
+        <div className="login-left">
+          <h1>Sign In</h1>
+          <form>
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input type="text" id="username" name="username" placeholder="Username" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" name="password" placeholder="Password" />
+            </div>
+            <button type="submit" className="login-btn">Sign In</button>
+            <div className="remember-forgot">
+              <label>
+                <input type="checkbox" /> Remember Me
+              </label>
+              <a href="#">Forgot Password</a>
+            </div>
+          </form>
         </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" />
+        <div className="login-right">
+          <h2>Welcome to login</h2>
+          <p>Don't have an account?</p>
+          <button className="signup-btn">Sign Up</button>
         </div>
-        <button type="submit" className="login-btn">Login</button>
-      </form>
+      </div>
     </div>
   );
 };
