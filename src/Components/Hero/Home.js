07d35fdf5133aad1087009db88112c81b0547039
thummasreddy <63746972+thummasreddy@users.noreply.github.com>
2024-05-30 09:28:15 -0400

    Updating

--- src/Components/Hero/Home.js
+++ src/Components/Hero/Home.js
@@ -5,10 +5,12 @@ import landingPageImage from '../Assets/Landing_Page.png'; // Import the image
 const Home = () => {
   return (
     <div className='home'>
+
+      <h3>We Supply Fresh Fruits and Vegetables</h3>
+      <h2>Fresh From Our Farms</h2>
       <img src={landingPageImage} alt="Landing Page" />
+
       <div>
-        <h3>We Supply Fresh Fruits and Vegetables</h3>
-        <h2>Fresh From Our Farms</h2>
         <p>Dive into a world of farm-fresh goodness. From the fields straight to your table, discover the joy of locally sourced produce delivered right to your doorstep. Join us in supporting local farmers and savoring the taste of freshness, one bite at a time.</p>
         <button className='shop-now-btn'>Shop Now</button>
       </div>
