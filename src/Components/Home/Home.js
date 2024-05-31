2bea878d2a4aa9cd4c28e7f682bd8482d5b56c29
thummasreddy <63746972+thummasreddy@users.noreply.github.com>
2024-05-31 15:13:22 -0400

    Working on Landing Page BG and it's CSS

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import './Home.css';
+import './Home.css'; // Import the CSS file
 import landingPageImage from '../Assets/Landing_Page.png'; // Import the image
+import landingPageBackground from '../Assets/Landing_Page_bg.png';
 
 const Home = () => {
   return (
     <div className='home-container'>
+      <img src={landingPageBackground} alt="Background" className='home-background-image' />
       <h3 className='home-title-small'>We Supply Fresh Fruits and Vegetables</h3>
       <h2 className='home-title-large'>Fresh From Our Farms</h2>
       <img src={landingPageImage} alt="Landing Page" className='home-image' />
       <div className='home-text'>
-        <p>Dive into a world of farm-fresh goodness. From the fields straight to your table, discover the joy of locally sourced produce delivered right to your doorstep. Join us in supporting local farmers and savoring the taste of freshness, one bite at a time.</p>
+        <p>Discover the joy of farm-fresh goodness, delivered straight to your table from local fields. Experience vibrant colors, crisp textures, and rich flavors in every bite. By choosing locally sourced produce, you support community farmers and contribute to a sustainable economy. Embrace the difference of truly fresh ingredients in your meals and feel good about nourishing your body and your community, one delicious bite at a time.</p>
         <button className='home-shop-now-btn'>Shop Now</button>
       </div>
       <div className='home-footer'>
