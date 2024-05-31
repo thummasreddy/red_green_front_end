235c30949af29a625bbd24fc95d179af8fc8d5b7
thummasreddy <63746972+thummasreddy@users.noreply.github.com>
2024-05-31 15:34:35 -0400

    Working on home page and CSS

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,17 +5,20 @@ import landingPageBackground from '../Assets/Landing_Page_bg.png';
 
 const Home = () => {
   return (
-    <div className='home-container'>
-      <img src={landingPageBackground} alt="Background" className='home-background-image' />
-      <h3 className='home-title-small'>We Supply Fresh Fruits and Vegetables</h3>
-      <h2 className='home-title-large'>Fresh From Our Farms</h2>
-      <img src={landingPageImage} alt="Landing Page" className='home-image' />
-      <div className='home-text'>
-        <p>Discover the joy of farm-fresh goodness, delivered straight to your table from local fields. Experience vibrant colors, crisp textures, and rich flavors in every bite. By choosing locally sourced produce, you support community farmers and contribute to a sustainable economy. Embrace the difference of truly fresh ingredients in your meals and feel good about nourishing your body and your community, one delicious bite at a time.</p>
-        <button className='home-shop-now-btn'>Shop Now</button>
-      </div>
-      <div className='home-footer'>
-        <h4>Get 25% Off on your first Purchase!</h4>
+    <div>
+    
+      <div className='home-container'>
+        <img src={landingPageBackground} alt="Background" className='home-background-image' />
+        <h3 className='home-title-small'>We Supply Fresh Fruits and Vegetables</h3>
+        <h2 className='home-title-large'>Fresh From Our Farms</h2>
+        <img src={landingPageImage} alt="Landing Page" className='home-image' />
+        <div className='home-text'>
+          <p>Discover the joy of farm-fresh goodness, delivered straight to your table from local fields. Experience vibrant colors, crisp textures, and rich flavors in every bite. By choosing locally sourced produce, you support community farmers and contribute to a sustainable economy. Embrace the difference of truly fresh ingredients in your meals and feel good about nourishing your body and your community, one delicious bite at a time.</p>
+          <button className='home-shop-now-btn'>Shop Now</button>
+        </div>
+        <div className='home-footer'>
+          <h4>Get 25% Off on your first Purchase!</h4>
+        </div>
       </div>
     </div>
   );
