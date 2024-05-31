a82421601419c6a83992bb7880dd8e36f649a559
thummasreddy <63746972+thummasreddy@users.noreply.github.com>
2024-05-31 13:28:58 -0400

    Worked on home css

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,15 +4,13 @@ import landingPageImage from '../Assets/Landing_Page.png'; // Import the image
 
 const Home = () => {
   return (
-    <div className='home'>
-
-      <h3>We Supply Fresh Fruits and Vegetables</h3>
-      <h2>Fresh From Our Farms</h2>
-      <img src={landingPageImage} alt="Landing Page" />
-
-      <div>
+    <div className='home-container'>
+      <h3 className='home-title-small'>We Supply Fresh Fruits and Vegetables</h3>
+      <h2 className='home-title-large'>Fresh From Our Farms</h2>
+      <img src={landingPageImage} alt="Landing Page" className='home-image' />
+      <div className='home-text'>
         <p>Dive into a world of farm-fresh goodness. From the fields straight to your table, discover the joy of locally sourced produce delivered right to your doorstep. Join us in supporting local farmers and savoring the taste of freshness, one bite at a time.</p>
-        <button className='shop-now-btn'>Shop Now</button>
+        <button className='home-shop-now-btn'>Shop Now</button>
       </div>
       <div className='home-footer'>
         <h4>Get 25% Off on your first Purchase!</h4>
