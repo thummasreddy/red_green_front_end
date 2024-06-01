c2d7f18b34fa2fe86b7a72f20ef51e155dd6659c
thummasreddy <63746972+thummasreddy@users.noreply.github.com>
2024-05-31 23:54:12 -0400

    Working on Home shop categories

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaTruck, FaLeaf, FaMedal, FaHeadset } from 'react-icons/fa';
 import './Home.css'; // Import the CSS file
 import landingPageImage from '../Assets/Landing_Page.png'; // Import the image
 import landingPageBackground from '../Assets/Landing_Page_bg.png';
@@ -6,7 +7,6 @@ import landingPageBackground from '../Assets/Landing_Page_bg.png';
 const Home = () => {
   return (
     <div>
-    
       <div className='home-container'>
         <img src={landingPageBackground} alt="Background" className='home-background-image' />
         <h3 className='home-title-small'>We Supply Fresh Fruits and Vegetables</h3>
@@ -18,6 +18,29 @@ const Home = () => {
         </div>
         <div className='home-footer'>
           <h4>Get 25% Off on your first Purchase!</h4>
+        </div>
+      </div>
+
+      <div className="features-container">
+        <div className="feature">
+          <FaTruck className="feature-icon" />
+          <h3>FREE SHIPPING</h3>
+          <p>ON ORDER OVER $100</p>
+        </div>
+        <div className="feature">
+          <FaLeaf className="feature-icon" />
+          <h3>ALWAYS FRESH</h3>
+          <p>PRODUCT WELL PACKAGE</p>
+        </div>
+        <div className="feature">
+          <FaMedal className="feature-icon" />
+          <h3>SUPERIOR QUALITY</h3>
+          <p>QUALITY PRODUCTS</p>
+        </div>
+        <div className="feature">
+          <FaHeadset className="feature-icon" />
+          <h3>SUPPORT</h3>
+          <p>24/7 SUPPORT</p>
         </div>
       </div>
     </div>
